f836a52c9bfab89a119a9e566fa40cbc73bbeed1
Mohit Soni <25.mohitsoni@gmail.com>
2021-09-07 21:17:25 +0530

    welcome screen added

---
+++ lib/constants/welcome_screen_data.dart
@@ -0,0 +1,26 @@
+List kWelcomeScreenSlidesData = [
+  {
+    "header": "Learn",
+    "index": 0,
+    "description":
+        "Online chat which provides its users maximum functionality to simplify the search",
+    "image":
+        "https://cdn.dribbble.com/users/1355613/screenshots/15681100/media/73d5063524dee8b7092a2f6dcd98babd.jpg"
+  },
+  {
+    "header": "Build",
+    "index": 1,
+    "description":
+        "Online chat which provides its users maximum functionality to simplify the search",
+    "image":
+        "https://cdn.dribbble.com/users/2417352/screenshots/14601875/media/061a551903f4e996c91febca13666612.png"
+  },
+  {
+    "header": "Launch",
+    "index": 2,
+    "description":
+        "Online chat which provides its users maximum functionality to simplify the search",
+    "image":
+        "https://cdn.dribbble.com/users/2417352/screenshots/14850541/media/ad0c31fd1d69e197923f2a904b2d18e1.png"
+  },
+];
---
+++ lib/ui/views/welcome_screen.dart
@@ -0,0 +1,129 @@
+import 'package:fitness_app_flutter/constants/welcome_screen_data.dart';
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  double? currentPage = 0.0;
+  final _pageViewController = new PageController();
+  List<Widget> slides = kWelcomeScreenSlidesData
+      .map(
+        (item) => Container(
+          padding: EdgeInsets.symmetric(horizontal: 18.0),
+          child: Column(
+            children: <Widget>[
+              Flexible(
+                flex: 1,
+                fit: FlexFit.tight,
+                child: Image.network(
+                  item['image'],
+                  fit: BoxFit.fitWidth,
+                  width: 220.0,
+                  alignment: Alignment.bottomCenter,
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                fit: FlexFit.tight,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 30.0),
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        item['header'],
+                        style: TextStyle(
+                          fontSize: 50.0,
+                          fontWeight: FontWeight.w300,
+                          color: Color(0XFF3F3D56),
+                          height: 2.0,
+                        ),
+                      ),
+                      Text(
+                        item['description'],
+                        style: TextStyle(
+                            color: Colors.grey,
+                            letterSpacing: 1.2,
+                            fontSize: 16.0,
+                            height: 1.3),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      )
+      .toList();
+
+  List<Widget> indicator() => List<Widget>.generate(
+        slides.length,
+        (index) => Container(
+          margin: EdgeInsets.symmetric(horizontal: 3.0),
+          height: 10.0,
+          width: 10.0,
+          decoration: BoxDecoration(
+            color: currentPage?.round() == index
+                ? Color(0XFF256075)
+                : Color(0XFF256075).withOpacity(0.2),
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Stack(
+          children: <Widget>[
+            PageView.builder(
+              controller: _pageViewController,
+              itemCount: slides.length,
+              itemBuilder: (BuildContext context, int index) {
+                _pageViewController.addListener(() {
+                  setState(() {
+                    currentPage = _pageViewController.page;
+                  });
+                });
+                return slides[index];
+              },
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                margin: EdgeInsets.only(top: 70.0),
+                padding: EdgeInsets.symmetric(vertical: 40.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: indicator(),
+                ),
+              ),
+            ),
+            currentPage == 2
+                ? Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Padding(
+                      padding: EdgeInsets.only(bottom: 108.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // TODO: Create a stateful file dashboard.dart and navigate to it
+                          print('Naviagte to Dashboard');
+                        },
+                        child: Text('Continue'),
+                      ),
+                    ),
+                  )
+                : Container()
+          ],
+        ),
+      ),
+    );
+  }
+}
