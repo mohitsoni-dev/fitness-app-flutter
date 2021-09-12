3615ecb1776bb9b440f14be66fe44e12ad2d7c84
Sushant Kumar <sushant21kumar@gmail.com>
2021-09-12 20:21:16 +0530

    three dot widget fixed

--- lib/ui/views/settings.dart
+++ lib/ui/views/settings.dart
@@ -12,29 +12,38 @@ class Settings extends StatefulWidget {
 class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+        foregroundColor: Colors.transparent,
+        actions: [
           threeDotWidget(),
-          //TODO profile pic to be added
-          profilePicWidget(),
-          //TODO user name to be added
-          userNameTextWidget("User Name"),
-          //TODO values to be added from database
-          midview(context),
-          DividerWidget(),
-          weeklyProgressTextWiget("Weekly Progress"),
-          //TODO progress value to be added from database
-          weeklyProgressBarWidget(),
-          //TODO Recent seven days of the week to be added from database
-          weekdaysTextwidget(),
-          DividerWidget(),
-          weeklyProgressTextWiget("Recent Exercise"),
-          //TODO recent exercise from database to be added
-          recentExerciseViewWidget(context)
-          //
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //TODO profile pic to be added
+            profilePicWidget(),
+            //TODO user name to be added
+            userNameTextWidget("User Name"),
+            //TODO values to be added from database
+            midview(context),
+            DividerWidget(),
+            weeklyProgressTextWiget("Weekly Progress"),
+            //TODO progress value to be added from database
+            weeklyProgressBarWidget(),
+            //TODO Recent seven days of the week to be added from database
+            weekdaysTextwidget(),
+            DividerWidget(),
+            weeklyProgressTextWiget("Recent Exercise"),
+            //TODO recent exercise from database to be added
+            recentExerciseViewWidget(context)
+//
+          ],
+        ),
       ),
     );
   }
