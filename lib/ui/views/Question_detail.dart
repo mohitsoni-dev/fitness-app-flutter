7a26b34908ac5a2f62ad6a198a335859e9d5aecf
Sushant Kumar <sushant21kumar@gmail.com>
2021-10-08 14:24:15 +0530

    question_detail.dart added

---
+++ lib/ui/views/Question_detail.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class CommunityPostAnswers extends StatelessWidget {
+  const CommunityPostAnswers({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
