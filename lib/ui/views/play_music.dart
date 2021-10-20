eaa7aaeb254a7df9b6b770c77d9be8a89b3714d4
Sushant Kumar <sushant21kumar@gmail.com>
2021-10-20 23:13:18 +0530

    sleep asmr completed

--- lib/ui/views/play_music.dart
+++ lib/ui/views/play_music.dart
@@ -1,40 +1,30 @@
-
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 //import 'package:assets_audio_player/assets_audio_player.dart';
 
 class PlayMusic extends StatefulWidget {
-  const PlayMusic({Key? key}) : super(key: key);
+  final String audioPath;
+  final String theme;
+  final String imageLink;
+
+  const PlayMusic(
+      {Key? key,
+      required this.audioPath,
+      required this.theme,
+      required this.imageLink})
+      : super(key: key);
 
   @override
   _PlayMusicState createState() => _PlayMusicState();
 }
 
 class _PlayMusicState extends State<PlayMusic> {
-  // AssetsAudioPlayer player= AssetsAudioPlayer();
   AudioPlayer audioPlayer = AudioPlayer();
 
   PlayerState playerState = PlayerState.PAUSED;
 
-  // AudioPlayerState audioPlayerState=AudioPlayerState.PAUSED;
   late AudioCache audioCache;
-  String path = "rain.mp3";
-  //
-  // int timeProgress = 0;
-  // int audioDuration = 0;
-  //
-  // Widget slider() {
-  //   return Container(
-  //     width: 300,
-  //     child: Slider.adaptive(
-  //         value: (timeProgress / 1000).floorToDouble(),
-  //         max: (audioDuration / 1000).floorToDouble(),
-  //         onChanged: (value) {
-  //           seekTosec(value.toInt());
-  //         }),
-  //   );
-  // }
 
   @override
   void initState() {
@@ -43,17 +33,14 @@ class _PlayMusicState extends State<PlayMusic> {
 
     // player.open(Audio('assets/rain.mp3'));
     audioCache = AudioCache(fixedPlayer: audioPlayer);
-    audioPlayer.onPlayerStateChanged.listen((PlayerState s) {
-      setState(() {
-        playerState = s;
-      });
-    });
 
-    // audioPlayer.onAudioPositionChanged.listen((Duration p)  async{
-    //   setState(() {
-    //     timeProgress = p.inMilliseconds;
-    //   });
-    // });
+    audioPlayer.onPlayerStateChanged.listen((PlayerState s) {
+      if (this.mounted) {
+        setState(() {
+          playerState = s;
+        });
+      }
+    });
   }
 
   @override
@@ -65,104 +52,81 @@ class _PlayMusicState extends State<PlayMusic> {
     audioCache.clearAll();
   }
 
+//Todo add timer
   playMusic() async {
-    await audioCache.play(path);
+    await audioCache.play(widget.audioPath);
   }
 
   pauseMusic() async {
     await audioPlayer.pause();
   }
 
-  // Widget getLocalFileDuration() {
-  //   return FutureBuilder<int>(
-  //     future: _getAudioDuration(),
-  //     builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
-  //       switch (snapshot.connectionState) {
-  //         case ConnectionState.none:
-  //           return Text('No Connection...');
-  //         case ConnectionState.active:
-  //         case ConnectionState.waiting:
-  //           return Text('Waiting...');
-  //         case ConnectionState.done:
-  //           if (snapshot.hasError) return Text('Error: ${snapshot.error}');
-  //           return Text(getTimeString(snapshot.data));
-  //       }
-  //        // unreachable
-  //     },
-  //   );
-  // }
-  //
-  //
-  //   Future<int> _getAudioDuration() async {
-  //   var audioFile = (await audioCache.load(path))   ;
-  //   await audioPlayer.setUrl(
-  //    audioFile.path ,
-  //   );
-  //
-  //   audioDuration = await Future.delayed(
-  //     Duration(seconds: 2),
-  //         () => audioPlayer.getDuration(),
-  //   );
-  //
-  //   return audioDuration;
-  //
-  // }
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            IconButton(
-                iconSize: 50,
-                onPressed: () {
-                  playerState == PlayerState.PLAYING
-                      ? pauseMusic()
-                      : playMusic();
-                },
-                icon: Icon(playerState == PlayerState.PLAYING
-                    ? Icons.pause
-                    : Icons.play_circle)),
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.center,
-            //   children: [
-            //     Text(getTimeString(timeProgress)),
-            //     SizedBox(
-            //       width: 10,
-            //     ),
-            //     slider(),
-            //     SizedBox(
-            //       width: 10,
-            //     ),
-            //     // Text(getTimeString(audioDuration)),
-            //     audioDuration==0? getLocalFileDuration():Text(getTimeString(audioDuration))
-            //
-            //   ],
-            //)
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color.fromRGBO(0, 147, 233, 0),
+              Color.fromRGBO(128, 208, 199, 100)
+            ],
+          )),
+          alignment: Alignment.center,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 12),
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Text(
+                      "Essence of nature - ${widget.theme}",
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: 1.5),
+                    ),
+                  ),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: Container(
+                        height: 280,
+                        width: 280,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          image: DecorationImage(
+                            image: NetworkImage(widget.imageLink),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                      iconSize: 50,
+                      onPressed: () {
+                        playerState == PlayerState.PLAYING
+                            ? pauseMusic()
+                            : playMusic();
+                      },
+                      icon: Icon(playerState == PlayerState.PLAYING
+                          ? Icons.pause
+                          : Icons.play_circle)),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
   }
-
-
-
-  // String getTimeString(int? milliseconds) {
-  //   if (milliseconds == null) milliseconds = 0;
-  //   String minutes =
-  //       '${(milliseconds / 60000).floor() < 10 ? 0 : ''}${(milliseconds / 60000).floor()}';
-  //   String seconds =
-  //       '${(milliseconds / 1000).floor() % 60 < 10 ? 0 : ''}${(milliseconds / 1000).floor() % 60}';
-  //   return '$minutes:$seconds'; // Returns a string with the format mm:ss
-  // }
-  //
-  // void seekTosec(int sec) {
-  //   Duration newPosition = Duration(seconds: sec);
-  //   audioPlayer.seek(newPosition);
-  // }
 }
