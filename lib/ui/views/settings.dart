c5a137c9f00265a95387c494a224a114f3497630
Sushant Kumar <sushant21kumar@gmail.com>
2021-11-24 12:01:01 +0530

    minor change

--- lib/ui/views/settings.dart
+++ lib/ui/views/settings.dart
@@ -90,7 +90,7 @@ class _SettingsState extends State<Settings> {
                   userNameTextWidget(userName, context),
 
                   midview(context, userWeight.toString(),
-                      bmi.toString().substring(0, 4), userHeight.toString()),
+                      bmi.toString().substring(0, 3), userHeight.toString()),
                   DividerWidget(),
                   //weeklyProgressTextWiget("Weekly Progress"),
                   //weeklyProgressBarWidget(),
