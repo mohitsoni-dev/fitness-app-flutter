2ac899ff75da96643f6e6d72d58b7adb9cf17e83
Sushant Kumar <sushant21kumar@gmail.com>
2021-11-24 19:44:45 +0530

    minor changes

--- lib/constants/welcome_screen_data.dart
+++ lib/constants/welcome_screen_data.dart
@@ -1,26 +1,26 @@
 const List kWelcomeScreenSlidesData = [
   {
-    "header": "Learn",
+    "header": "Exercise",
     "index": 0,
     "description":
-        "Online chat which provides its users maximum functionality to simplify the search",
+        "Physical activity guidelines for Everyone· Do strengthening activities that work all the major muscle groups (legs, chest, shoulder, arms, ...",
     "image":
-        "https://cdn.dribbble.com/users/1355613/screenshots/15681100/media/73d5063524dee8b7092a2f6dcd98babd.jpg"
+        "https://health.clevelandclinic.org/wp-content/uploads/sites/3/2013/09/inexpensiveExercise-1277759983-770x533-1.jpg"
   },
   {
-    "header": "Build",
+    "header": "Learn",
     "index": 1,
     "description":
-        "Online chat which provides its users maximum functionality to simplify the search",
+        "Know more about your body,fitness and well being.Keep your fitness knowledge updated",
     "image":
-        "https://cdn.dribbble.com/users/2417352/screenshots/14601875/media/061a551903f4e996c91febca13666612.png"
+        "https://edsurge.imgix.net/uploads/post/image/13679/brain_unplug-1600291328.jpg?auto=compress%2Cformat&w=640&h=259&fit=crop"
   },
   {
-    "header": "Launch",
+    "header": "Ask",
     "index": 2,
     "description":
-        "Online chat which provides its users maximum functionality to simplify the search",
+        "Ask your every fitness and health related doubt and get answers from top experts around.Even the smallest query will be answered.  ",
     "image":
-        "https://cdn.dribbble.com/users/2417352/screenshots/14850541/media/ad0c31fd1d69e197923f2a904b2d18e1.png"
+        "https://www.thoughtco.com/thmb/tX0rHqoj9w7DzGiBhSSPVY1rfro=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/woman-with-a-question-mark-above-her-head-109742961-5a70cd9c1d6404003720b5aa.jpg"
   },
 ];
